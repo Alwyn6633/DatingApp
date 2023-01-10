81110ba2ec3d384095b85a5310e153c9f5c7abf2
Alwyn <alwyn@ipingroup.com>
2023-01-10 16:51:41 +0200

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations;
+using System.Security;
+
 namespace API.Entities
 {
     public class AppUser
@@ -5,5 +8,9 @@ namespace API.Entities
         public int Id { get; set; }
 
         public string UserName { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
     }
 }
